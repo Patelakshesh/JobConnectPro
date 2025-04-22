e35e8a637c3e134f57e137597a43f5ae0843935f
akshesh patel <aksheshp@gmail.com>
2025-04-22 10:41:21 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 const corsOption = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobconnectpro-6ix7.onrender.com',
     credentials: true   
 }
 app.use(cors(corsOption))
